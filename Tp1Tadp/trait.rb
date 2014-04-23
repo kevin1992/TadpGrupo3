2fc1f2522de577cbdc5a48a50e173c0dd1e08354
ministeriodeindustria <ministeriodeindustriagithub@gmail.com>
2014-04-23 14:55:18 -0300

    Empezando con el framework

---
+++ Tp1Tadp/trait.rb
@@ -0,0 +1,87 @@
+#TODO hacer que se banque metodos con parametros
+
+class Trait
+
+  def self.define(nombre,&bloqueMetodos)
+
+    nuevoTrait = Object.const_set(nombre,TraitOperador.new)
+
+
+    class << nuevoTrait
+
+      @@metodosAgregados = []
+
+      def agregarMethod(nombre, &bloque)
+        self.class.instance_eval do
+          define_method(nombre) {bloque.yield}
+          @@metodosAgregados.push (nombre)
+        end
+      end
+
+      def metodosAgregados
+        @@metodosAgregados
+      end
+
+    end
+
+
+    nuevoTrait.instance_eval(&bloqueMetodos)
+
+
+
+  end
+
+end
+
+
+
+class TraitOperador
+
+end
+
+
+
+class Object
+
+  def uses trait, lista=nil
+
+    if lista == nil
+      lista=trait.metodosAgregados
+  else
+    lista = lista.select {|elem| trait.metodosAgregados.include? elem }
+  end
+
+    lista.each {|nombreMetodo| define_method(nombreMetodo)  {trait.method(nombreMetodo).call}}
+
+  end
+
+
+end
+
+
+
+
+Trait.define('MiTrait') do
+
+  agregarMethod :saludar do
+        puts "Hola!"
+  end
+
+  agregarMethod :sumar do
+    puts "5"
+  end
+
+
+end
+
+
+class Persona
+  uses MiTrait
+
+end
+
+
+p = Persona.new
+
+p.saludar()
+p.sumar()
