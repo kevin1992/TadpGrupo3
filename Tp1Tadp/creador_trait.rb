67b0144bb0d96ae5eab22b090138a674469f3838
kevin <kevin1992@gmail.com>
2014-05-14 22:53:02 -0300

    Agregado nuevos metodos + y -

--- Tp1Tadp/creador_trait.rb
+++ Tp1Tadp/creador_trait.rb
@@ -1,11 +1,11 @@
 class CreadorTrait
 
-  def self.definirTrait(nombre,estrategia=EstrategiaNoHacerNada.new,&bloqueMetodos)
+  def self.definirTrait(nombre,&bloqueMetodos)
 
     nuevoTrait = Object.const_set(nombre,Trait.new)
     nuevoTrait.nombre = nombre
     nuevoTrait.agregarMetodos &bloqueMetodos
-    nuevoTrait.estrategia = estrategia
+
 
   end
 
--- Tp1Tadp/main.rb
+++ Tp1Tadp/main.rb
@@ -5,89 +5,33 @@ require_relative 'framework'
 
 
 
-CreadorTrait.definirTrait('MiTrait',EstrategiaTodosLosMensajes.new()) do
+CreadorTrait.definirTrait('MiTrait') do
 
   agregarMethod :saludar do
 
-    'Hola!' + self.nombre
+    'Hola! ' + self.nombre
   end
 
 
-  agregarMethod :chau do
+  agregarMethod :chau do  |nombre|
      'chau! '+nombre.to_s
   end
 
 end
 
-CreadorTrait.definirTrait('MiOtroTrait',EstrategiaTodosLosMensajes.new()) do
-
-  agregarMethod :saludar do
-    'conflicto mi otro trait'
-  end
-  agregarMethod :hablar do ||
-   'blabla '+nombre.to_s
-  end
-
-
-end
-
-estrategia = EstrategiaPorCorte.new() {|elem| elem>0 }
-
-=begin
-CreadorTrait.definirTrait('Trait1',estrategia  ) do
-
-  agregarMethod :saltar do
-   1
-  end
-end
-CreadorTrait.definirTrait('Trait2',estrategia  ) do
-
-  agregarMethod :saltar do
-  -2
-  end
-end
-CreadorTrait.definirTrait('Trait3',estrategia ) do
-
-  agregarMethod :saltar do
-  -3
-  end
-end
-
-CreadorTrait.definirTrait('Trait4',estrategia ) do
-
-  agregarMethod :saltar do
-   -5
-  end
-end
-=end
 
 
 class Persona
-  #uses MiTrait << ('saludar,saludo') Forma de usar el <<
-  uses MiTrait+MiOtroTrait
+
+  uses MiTrait
   attr_accessor :nombre
   def initialize(nombre)
     self.nombre = nombre
     end
 
-end    #tira TraitException por :saludar
+end
 
 p= Persona.new("kevin")
 
 puts p.saludar
-puts p.chau
-
-# class Persona
-#   uses (MiTrait+MiOtroTrait){
-#     resolver_con(EstrategiaTodosLosMensajes.new, :saludar, :mitrait_saludar)
-#   }
-#   attr_accessor :nombre
-#   def initialize(nombre)
-#     self.nombre = nombre
-#   end
-#
-# end    #sintaxis para resolver conflictos
-#
-#
-#
-#
+puts p.chau('kevin')
